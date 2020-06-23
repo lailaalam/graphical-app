74db3a00f4b7b43f8436d71b771949ce908b0cc9
Laila Alam <alamlaila09@gmail.com>
2020-06-23 17:10:12 +0600

    Third commit of bfs with level print

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,20 +12,24 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit {
   private Thing: string[][];
   private visited: boolean[][];
+  private level: number[][];
+
 
   constructor() {
     this.Thing = [];
     this.visited = [];
-
+    this.level = [];
   }
 
   ngOnInit() {
     for (let i = 0; i < 16; i++) {
       this.Thing[i] = [];
       this.visited[i] = [];
+      this.level[i] = [];
       for (let j = 0; j < 16; j++) {
         this.Thing[i][j] = 'card';
         this.visited[i][j] = false;
+        this.level[i][j] = 0;
       }
     }
     //this.Thing[1][1] = 'card1';
@@ -45,6 +49,7 @@ export class AppComponent implements OnInit {
       let Q = [];
       Q.push([1 , 3]);
       this.visited[1][3] = true;
+      this.level[1][3] = 0;
       this.Thing[1][3] = 'cng';
       while (Q.length > 0) {
        let x = Q[0];
@@ -55,6 +60,7 @@ export class AppComponent implements OnInit {
         j = x[1];
         if (i >= 0 && i < 16 && j >= 0 && j < 16 && this.visited[i][j] == false) {
           this.visited[i][j] = true;
+          this.level[i][j] = this.level[x[0]][x[1]] + 1;
           if (i == 5 && j == 8) break;
           this.Thing[i][j] = 'cng';
           Q.push([i, j]);
@@ -65,6 +71,7 @@ export class AppComponent implements OnInit {
         j = x[1];
         if (i >= 0 && i < 16 && j >= 0 && j < 16 && this.visited[i][j] == false) {
           this.visited[i][j] = true;
+          this.level[i][j] = this.level[x[0]][x[1]] + 1;
           if (i == 5 && j == 8) break;
           this.Thing[i][j] = 'cng';
           Q.push([i, j]);
@@ -75,6 +82,7 @@ export class AppComponent implements OnInit {
         j = x[1] + 1;
         if (i >= 0 && i < 16 && j >= 0 && j < 16 && this.visited[i][j] == false) {
           this.visited[i][j] = true;
+          this.level[i][j] = this.level[x[0]][x[1]] + 1;
           if (i == 5 && j == 8) break;
           this.Thing[i][j] = 'cng';
           Q.push([i, j]);
@@ -86,6 +94,7 @@ export class AppComponent implements OnInit {
         j = x[1] - 1;
         if (i >= 0 && i < 16 && j >= 0 && j < 16 && this.visited[i][j] == false) {
           this.visited[i][j] = true;
+          this.level[i][j] = this.level[x[0]][x[1]] + 1;
           if (i == 5 && j == 8) break;
           this.Thing[i][j] = 'cng';
           Q.push([i, j]);
@@ -94,6 +103,7 @@ export class AppComponent implements OnInit {
         }
         this.Thing[x[0]][x[1]] = 'cng';
       }
+      this.Thing[1][3] = 'startnode';
 
     }
   }
